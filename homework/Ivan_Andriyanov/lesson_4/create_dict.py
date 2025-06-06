e3cb0752b979240928efaa1298796d9575947ce5
Andriyanov Ivan <Poke37454@yandex.ru>
2025-06-06 10:18:26 +0300

    Исправил ошибки

--- homework/Ivan_Andriyanov/lesson_4/create_dict.py
+++ homework/Ivan_Andriyanov/lesson_4/create_dict.py
@@ -11,12 +11,12 @@ my_dict = {
     "set": {10, 20, 30, 40, 50}
 }
 
-print(f'последний элемент что хранится под ключом "tuple" : {my_dict["tuple"][4]}')
+print(f'последний элемент что хранится под ключом "tuple" : {my_dict["tuple"][-1]}')
 my_dict["list"].append("tiger")
 print(f'добавлен последний элемент в спискок что хранится под ключом "list" : {my_dict["list"]}')
 my_dict["list"].pop(1)
 print(f'Удален второй элемент списка что хранится под ключом "list" : {my_dict["list"]}')
-my_dict["dict"]["i am a tuple"] = "Добавил значение"
+my_dict["dict"][('i am a tuple',)] = "Добавил значение"
 print(f'Добавил ключ и значение {my_dict["dict"]}')
 del my_dict["dict"]["dog"]
 print(f'удалил эллемент dog {my_dict["dict"]}')
