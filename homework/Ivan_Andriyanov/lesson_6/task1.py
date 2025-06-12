f8b927f74b8b1b843a8de1352433d19846e4d61b
Andriyanov Ivan <Poke37454@yandex.ru>
2025-06-12 13:35:40 +0300

    finish task 6

---
+++ homework/Ivan_Andriyanov/lesson_6/task1.py
@@ -0,0 +1,16 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+new_text = text.split()
+
+list_text = []
+
+for word in new_text:
+    if word.endswith(',') or word.endswith('.'):
+        sign = word[-1]
+        clean_text = word[:-1]
+        new_word = clean_text + "ing" + sign
+    else:
+        new_word = word + "ing"
+    list_text.append(new_word)
+result = ' '.join(list_text)
+print(result)
