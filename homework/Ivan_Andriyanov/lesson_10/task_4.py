68ecf796c13072789ab0440d0a46218adde6937e
Andriyanov Ivan <Poke37454@yandex.ru>
2025-06-28 15:10:25 +0300

    add task_4

---
+++ homework/Ivan_Andriyanov/lesson_10/task_4.py
@@ -0,0 +1,12 @@
+PRICE_LIST = '''тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р'''
+
+data = dict((line.split()[0], int(line.split()[1][:-1]))
+             for line in PRICE_LIST.splitlines())
+
+print(data)
