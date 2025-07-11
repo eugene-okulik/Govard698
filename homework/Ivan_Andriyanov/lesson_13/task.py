dfc5b627d06c04e7acbe8c1aef09460342ef7ccd
Andriyanov Ivan <Poke37454@yandex.ru>
2025-07-11 17:32:34 +0300

    test71

--- homework/Ivan_Andriyanov/lesson_13/task.py
+++ homework/Ivan_Andriyanov/lesson_13/task.py
@@ -14,14 +14,12 @@ def read_file():
             yield line
 
 
-
 def extract_datetime_from_line(line):
     match = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+", line)
     if match:
         date_str = match.group()
         return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
     return None
-
 
 
 def change_stings():
