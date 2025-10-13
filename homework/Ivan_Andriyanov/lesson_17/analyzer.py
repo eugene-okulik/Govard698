cbba8c435fecda5395277c355358a37b6f3b58f0
Andriyanov Ivan <Poke37454@yandex.ru>
2025-10-13 22:49:26 +0300

    Fix flake8

--- homework/Ivan_Andriyanov/lesson_17/analyzer.py
+++ homework/Ivan_Andriyanov/lesson_17/analyzer.py
@@ -1,4 +1,6 @@
-import argparse, os
+import argparse
+import os
+
 
 parser = argparse.ArgumentParser(description="Поиск текста в файлах")
 parser.add_argument("log_dir", help="путь к папке с логами")
@@ -10,10 +12,10 @@ log_dir = args.log_dir
 for file_name in os.listdir(log_dir):
     file_path = os.path.join(log_dir, file_name)
     if os.path.isfile(file_path):
-        print('file find:', file_path)
+        print("file find:", file_path)
 
         # читаем файл построчно и ищем совпадения
-        with open(file_path, 'r', encoding="utf-8", errors="ignore") as f:
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
             for line_number, line in enumerate(f, start=1):
                 if args.text in line:
                     words = line.strip().split()
@@ -22,6 +24,7 @@ for file_name in os.listdir(log_dir):
                             start = max(0, idx - 5)
                             end = min(len(words), idx + 6)
                             snippet = " ".join(words[start:end])
-                            print(f'[{file_name}] строка {line_number}: {snippet}')
-print("holder: ", args.log_dir)
-print("Что ищем: ", args.text)
+                            print(f"[{file_name}] строка {line_number}: {snippet}")
+
+print("holder:", args.log_dir)
+print("Что ищем:", args.text)
